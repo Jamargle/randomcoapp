89a27293da14daa38f611864d9d3f6e2356e99bd
Jose <jmlb0003@gmail.com>
2018-11-11 19:47:41 +0100

    Added more details to the details screen

--- app/src/main/java/com/jmlb0003/randomcoapp/presentation/details/DetailsFragment.kt
+++ app/src/main/java/com/jmlb0003/randomcoapp/presentation/details/DetailsFragment.kt
@@ -5,6 +5,14 @@ import com.jmlb0003.randomcoapp.R
 import com.jmlb0003.randomcoapp.app.di.PresenterFactory
 import com.jmlb0003.randomcoapp.domain.model.User
 import com.jmlb0003.randomcoapp.presentation.BaseFragment
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_details.location_line1
+import kotlinx.android.synthetic.main.fragment_details.location_line2
+import kotlinx.android.synthetic.main.fragment_details.registered_date
+import kotlinx.android.synthetic.main.fragment_details.user_email
+import kotlinx.android.synthetic.main.fragment_details.user_gender
+import kotlinx.android.synthetic.main.fragment_details.user_name
+import kotlinx.android.synthetic.main.fragment_details.user_picture
 
 class DetailsFragment : BaseFragment<DetailsFragment.Callback, DetailsPresenter.DetailsView, DetailsPresenter>(),
                         DetailsPresenter.DetailsView {
@@ -16,6 +24,12 @@ class DetailsFragment : BaseFragment<DetailsFragment.Callback, DetailsPresenter.
 
     override fun getMVPViewReference(): DetailsPresenter.DetailsView = this
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val user = arguments?.getParcelable<User>(USER_TO_SHOW)
+        getPresenter().onViewInitialized(user)
+    }
+
     override fun showLoading() {
         callback?.onShowLoading()
     }
@@ -24,8 +38,32 @@ class DetailsFragment : BaseFragment<DetailsFragment.Callback, DetailsPresenter.
         callback?.onHideLoading()
     }
 
-    override fun showUserDetails() {
-        
+    override fun showUserName(name: String, surname: String) {
+        user_name.text = "$name $surname"
+    }
+
+    override fun showUserEmail(email: String) {
+        user_email.text = email
+    }
+
+    override fun showUserAddress(street: String, city: String, latitude: String, longitude: String) {
+        location_line1.text = "$street, $city"
+        location_line2.text = "Lat: $latitude  Lon: $longitude"
+    }
+
+    override fun showRegisterDate(parsedDate: String) {
+        registered_date.text = "Registered since: $parsedDate"
+    }
+
+    override fun showGender(gender: String) {
+        user_gender.text = gender
+    }
+
+    override fun showPicture(picturePath: String) {
+        Picasso.with(activity)
+            .load(picturePath)
+            .fit().centerInside()
+            .into(user_picture)
     }
 
     override fun showError(errorMessage: String) {
--- app/src/main/java/com/jmlb0003/randomcoapp/presentation/details/DetailsPresenter.kt
+++ app/src/main/java/com/jmlb0003/randomcoapp/presentation/details/DetailsPresenter.kt
@@ -1,19 +1,44 @@
 package com.jmlb0003.randomcoapp.presentation.details
 
 import com.jmlb0003.randomcoapp.app.ErrorHandler
+import com.jmlb0003.randomcoapp.domain.model.User
 import com.jmlb0003.randomcoapp.presentation.BasePresenter
 import com.jmlb0003.randomcoapp.presentation.BasePresenterImp
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class DetailsPresenter(private val errorHandler: ErrorHandler) : BasePresenterImp<DetailsPresenter.DetailsView>() {
 
     override fun getErrorHandler(): ErrorHandler? = errorHandler
 
-    fun onViewInitialized() {
-        getView()?.let {
-            it.showLoading()
-            it.showUserDetails()
+    fun onViewInitialized(user: User?) {
+        getView()?.let { view ->
+            view.showLoading()
+
+            user?.let {
+                view.showUserName(user.name, user.surname)
+                view.showUserEmail(user.email)
+                with(user.location) {
+                    view.showUserAddress(street ?: "", city ?: "", latitude ?: "", longitude ?: "")
+                }
+
+                val date = parseDate(user.registeredDate)
+                if (!date.isBlank()) {
+                    view.showRegisterDate(date)
+                }
+                view.showGender(user.gender.description)
+                user.picture?.let { picture -> view.showPicture(picture) }
+            }
+
+            view.hideLoading()
         }
     }
+
+    private fun parseDate(registeredDate: Date?): String =
+        registeredDate?.let {
+            SimpleDateFormat("dd MM yyyy", Locale.getDefault()).format(registeredDate)
+        } ?: ""
 
     interface DetailsView : BasePresenter.BaseView {
 
@@ -21,7 +46,17 @@ class DetailsPresenter(private val errorHandler: ErrorHandler) : BasePresenterIm
 
         fun hideLoading()
 
-        fun showUserDetails()
+        fun showUserName(name: String, surname: String)
+
+        fun showUserEmail(email: String)
+
+        fun showUserAddress(street: String, city: String, latitude: String, longitude: String)
+
+        fun showRegisterDate(parsedDate: String)
+
+        fun showGender(gender: String)
+
+        fun showPicture(picturePath: String)
 
         fun showError(errorMessage: String)
 
